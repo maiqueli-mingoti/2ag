0dcc9e7e0c86fcf2c7e7fc9432665b7b5cfe415d
CarolPiazza <caroline.piazza@estudante.uffs.edu.br>
2025-06-16 21:15:41 -0300

    criado arquivos da pagina de acompanhamento paciente

--- 2ag-front/src/pages/AcompanhamentoPaciente/AcompanhamentoPaciente.jsx
+++ 2ag-front/src/pages/AcompanhamentoPaciente/AcompanhamentoPaciente.jsx
@@ -31,7 +31,6 @@ export default function AcompanhamentoPaciente() {
 
     const handleSalvar = () => {
         console.log("Salvando acompanhamento:", formData);
-        console.log("Arquivos:", arquivos);
     };
 
     const opcoes = {

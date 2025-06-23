2e269aead9fc725f6227dbb97d76f79785300b2e
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2025-06-23 00:36:16 -0300

    feat(backend): implementa atributos completos para as classes FollowUp, Hamilton e MEEM

--- backend/doisag/src/main/java/dev/uffs/doisag/model/FollowUp.java
+++ backend/doisag/src/main/java/dev/uffs/doisag/model/FollowUp.java
@@ -5,5 +5,209 @@ import java.time.LocalDate;
 
 @Entity
 public class FollowUp extends BaseAssessment {
+    private int morningDrops;
+    private int afternoonDrops;
+    private String comment;
+    private int tremor;
+    private int rigiditySpasticity;
+    private int nausea;
+    private int concentration;
+    private int appetite;
+    private int socialInteraction;
+    private int disposition;
+    private int intestinalFunction;
+    private int anxiety;
+    private int substanceReduction;
+    private int pain;
+    private int sportsPerformance;
+    private int sleep;
+    private int dermatologicalDisease;
+    private int mood;
 
+    public FollowUp(Long id, LocalDate assessmentDate, Patient patient, int morningDrops, int afternoonDrops, String comment, int tremor, int rigiditySpasticity, int nausea, int concentration, int appetite, int socialInteraction, int disposition, int anxiety, int intestinalFunction, int substanceReduction, int pain, int sportsPerformance, int sleep, int dermatologicalDisease, int mood) {
+        super(id, assessmentDate, patient);
+        this.morningDrops = morningDrops;
+        this.afternoonDrops = afternoonDrops;
+        this.comment = comment;
+        this.tremor = tremor;
+        this.rigiditySpasticity = rigiditySpasticity;
+        this.nausea = nausea;
+        this.concentration = concentration;
+        this.appetite = appetite;
+        this.socialInteraction = socialInteraction;
+        this.disposition = disposition;
+        this.anxiety = anxiety;
+        this.intestinalFunction = intestinalFunction;
+        this.substanceReduction = substanceReduction;
+        this.pain = pain;
+        this.sportsPerformance = sportsPerformance;
+        this.sleep = sleep;
+        this.dermatologicalDisease = dermatologicalDisease;
+        this.mood = mood;
+    }
+    // comentario opcional
+    public FollowUp(Long id, LocalDate assessmentDate, Patient patient, int morningDrops, int afternoonDrops, int tremor, int nausea, int rigiditySpasticity, int concentration, int appetite, int socialInteraction, int disposition, int intestinalFunction, int anxiety, int pain, int substanceReduction, int sleep, int sportsPerformance, int dermatologicalDisease, int mood) {
+        super(id, assessmentDate, patient);
+        this.morningDrops = morningDrops;
+        this.afternoonDrops = afternoonDrops;
+        this.tremor = tremor;
+        this.nausea = nausea;
+        this.rigiditySpasticity = rigiditySpasticity;
+        this.concentration = concentration;
+        this.appetite = appetite;
+        this.socialInteraction = socialInteraction;
+        this.disposition = disposition;
+        this.intestinalFunction = intestinalFunction;
+        this.anxiety = anxiety;
+        this.pain = pain;
+        this.substanceReduction = substanceReduction;
+        this.sleep = sleep;
+        this.sportsPerformance = sportsPerformance;
+        this.dermatologicalDisease = dermatologicalDisease;
+        this.mood = mood;
+    }
+
+    public int getMorningDrops() {
+        return morningDrops;
+    }
+
+    public void setMorningDrops(int morningDrops) {
+        this.morningDrops = morningDrops;
+    }
+
+    public int getAfternoonDrops() {
+        return afternoonDrops;
+    }
+
+    public void setAfternoonDrops(int afternoonDrops) {
+        this.afternoonDrops = afternoonDrops;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getTremor() {
+        return tremor;
+    }
+
+    public void setTremor(int tremor) {
+        this.tremor = tremor;
+    }
+
+    public int getRigiditySpasticity() {
+        return rigiditySpasticity;
+    }
+
+    public void setRigiditySpasticity(int rigiditySpasticity) {
+        this.rigiditySpasticity = rigiditySpasticity;
+    }
+
+    public int getNausea() {
+        return nausea;
+    }
+
+    public void setNausea(int nausea) {
+        this.nausea = nausea;
+    }
+
+    public int getConcentration() {
+        return concentration;
+    }
+
+    public void setConcentration(int concentration) {
+        this.concentration = concentration;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    public int getDisposition() {
+        return disposition;
+    }
+
+    public void setDisposition(int disposition) {
+        this.disposition = disposition;
+    }
+
+    public int getSocialInteraction() {
+        return socialInteraction;
+    }
+
+    public void setSocialInteraction(int socialInteraction) {
+        this.socialInteraction = socialInteraction;
+    }
+
+    public int getIntestinalFunction() {
+        return intestinalFunction;
+    }
+
+    public void setIntestinalFunction(int intestinalFunction) {
+        this.intestinalFunction = intestinalFunction;
+    }
+
+    public int getAnxiety() {
+        return anxiety;
+    }
+
+    public void setAnxiety(int anxiety) {
+        this.anxiety = anxiety;
+    }
+
+    public int getSubstanceReduction() {
+        return substanceReduction;
+    }
+
+    public void setSubstanceReduction(int substanceReduction) {
+        this.substanceReduction = substanceReduction;
+    }
+
+    public int getPain() {
+        return pain;
+    }
+
+    public void setPain(int pain) {
+        this.pain = pain;
+    }
+
+    public int getSportsPerformance() {
+        return sportsPerformance;
+    }
+
+    public void setSportsPerformance(int sportsPerformance) {
+        this.sportsPerformance = sportsPerformance;
+    }
+
+    public int getSleep() {
+        return sleep;
+    }
+
+    public void setSleep(int sleep) {
+        this.sleep = sleep;
+    }
+
+    public int getDermatologicalDisease() {
+        return dermatologicalDisease;
+    }
+
+    public void setDermatologicalDisease(int dermatologicalDisease) {
+        this.dermatologicalDisease = dermatologicalDisease;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
 }
--- backend/doisag/src/main/java/dev/uffs/doisag/model/MentalStateExam.java
+++ backend/doisag/src/main/java/dev/uffs/doisag/model/MentalStateExam.java
@@ -1,7 +1,115 @@
 package dev.uffs.doisag.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class MentalStateExam {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appointment_id", nullable = false) // fk para a consulta
+    private Appointment appointment;
+    private int temporalOrientation;
+    private int spatialOrientation;
+    private int registration;
+    private int attentionAndCalculation;
+    private int recall;
+    private int naming;
+    private int repetition;
+    private int command;
+    private int score;
+
+    public MentalStateExam(Appointment appointment, int temporalOrientation, int spatialOrientation, int registration, int attentionAndCalculation, int recall, int naming, int repetition, int command, int score) {
+        this.appointment = appointment;
+        this.temporalOrientation = temporalOrientation;
+        this.spatialOrientation = spatialOrientation;
+        this.registration = registration;
+        this.attentionAndCalculation = attentionAndCalculation;
+        this.recall = recall;
+        this.naming = naming;
+        this.repetition = repetition;
+        this.command = command;
+        this.score = score;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
+
+    public int getTemporalOrientation() {
+        return temporalOrientation;
+    }
+
+    public void setTemporalOrientation(int temporalOrientation) {
+        this.temporalOrientation = temporalOrientation;
+    }
+
+    public int getSpatialOrientation() {
+        return spatialOrientation;
+    }
+
+    public void setSpatialOrientation(int spatialOrientation) {
+        this.spatialOrientation = spatialOrientation;
+    }
+
+    public int getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(int registration) {
+        this.registration = registration;
+    }
+
+    public int getAttentionAndCalculation() {
+        return attentionAndCalculation;
+    }
+
+    public void setAttentionAndCalculation(int attentionAndCalculation) {
+        this.attentionAndCalculation = attentionAndCalculation;
+    }
+
+    public int getRecall() {
+        return recall;
+    }
+
+    public void setRecall(int recall) {
+        this.recall = recall;
+    }
+
+    public int getNaming() {
+        return naming;
+    }
+
+    public void setNaming(int naming) {
+        this.naming = naming;
+    }
+
+    public int getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(int repetition) {
+        this.repetition = repetition;
+    }
+
+    public int getCommand() {
+        return command;
+    }
+
+    public void setCommand(int command) {
+        this.command = command;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }

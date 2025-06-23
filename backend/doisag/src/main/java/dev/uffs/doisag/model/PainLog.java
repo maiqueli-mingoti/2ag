5ea9b2061308ab035e3cc8f752ef986ba2d54e25
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2025-06-23 00:47:48 -0300

    feat(backend): cria atributos para a classe PainLog

--- backend/doisag/src/main/java/dev/uffs/doisag/model/PainLog.java
+++ backend/doisag/src/main/java/dev/uffs/doisag/model/PainLog.java
@@ -1,7 +1,84 @@
 package dev.uffs.doisag.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+
+import java.time.LocalDate;
 
 @Entity
-public class PainLog {
+public class PainLog extends BaseAssessment {
+    private int basicActivityInterference;
+    private int socialActivityInterference;
+    private int sleepInterference;
+    private int productivityInterference;
+    private int extraMedication;
+    @Lob // campo longo
+    private String observation;
+
+    public PainLog(Long id, LocalDate assessmentDate, Patient patient, int socialActivityInterference, int basicActivityInterference, int sleepInterference, int productivityInterference, int extraMedication, String observation) {
+        super(id, assessmentDate, patient);
+        this.socialActivityInterference = socialActivityInterference;
+        this.basicActivityInterference = basicActivityInterference;
+        this.sleepInterference = sleepInterference;
+        this.productivityInterference = productivityInterference;
+        this.extraMedication = extraMedication;
+        this.observation = observation;
+    }
+        // observacao eh opcional
+    public PainLog(Long id, LocalDate assessmentDate, Patient patient, int basicActivityInterference, int extraMedication, int productivityInterference, int sleepInterference, int socialActivityInterference) {
+        super(id, assessmentDate, patient);
+        this.basicActivityInterference = basicActivityInterference;
+        this.extraMedication = extraMedication;
+        this.productivityInterference = productivityInterference;
+        this.sleepInterference = sleepInterference;
+        this.socialActivityInterference = socialActivityInterference;
+    }
+
+    public int getSocialActivityInterference() {
+        return socialActivityInterference;
+    }
+
+    public void setSocialActivityInterference(int socialActivityInterference) {
+        this.socialActivityInterference = socialActivityInterference;
+    }
+
+    public int getBasicActivityInterference() {
+        return basicActivityInterference;
+    }
+
+    public void setBasicActivityInterference(int basicActivityInterference) {
+        this.basicActivityInterference = basicActivityInterference;
+    }
+
+    public int getSleepInterference() {
+        return sleepInterference;
+    }
+
+    public void setSleepInterference(int sleepInterference) {
+        this.sleepInterference = sleepInterference;
+    }
+
+    public int getProductivityInterference() {
+        return productivityInterference;
+    }
+
+    public void setProductivityInterference(int productivityInterference) {
+        this.productivityInterference = productivityInterference;
+    }
+
+    public int getExtraMedication() {
+        return extraMedication;
+    }
+
+    public void setExtraMedication(int extraMedication) {
+        this.extraMedication = extraMedication;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
 }

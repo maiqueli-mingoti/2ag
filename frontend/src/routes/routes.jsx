9358680a76ae3b32af4f068dea847d9b207b9087
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2025-06-28 18:10:31 -0300

    fix(routes): comenta temporariamente a rota para a tela de agendamento para permitir a execução do projeto

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,6 +1,8 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
-import AcompanhamentoSemanalPaciente from "../pages/acompanhamento-semanal-paciente/acompanhamento-semanal-paciente.jsx";
-import AcompanhamentoSemanalPrescritor from "../pages/acompanhamento-semanal-prescritor/acompanhamento-semanal-prescritor.jsx";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router";
+import AcompanhamentoSemanalPaciente
+    from "../pages/acompanhamento-semanal-paciente/acompanhamento-semanal-paciente.jsx";
+import AcompanhamentoSemanalPrescritor
+    from "../pages/acompanhamento-semanal-prescritor/acompanhamento-semanal-prescritor.jsx";
 import ConsultaClinica from "../pages/consulta-clinica/consulta-clinica.jsx";
 import DadosConsultorio from "../pages/dados-consultorio/dados-consultorio.jsx";
 import DashboardPaciente from "../pages/dashboard-paciente/dashboard-paciente.jsx";
@@ -8,59 +10,60 @@ import DashboardPrescritor from "../pages/dashboard-prescritor/dashboard-prescri
 import Login from "../pages/login/login.jsx";
 import Prescricao from "../pages/prescricao/prescricao.jsx";
 import SignUp from "../pages/sign-up/sign-up.jsx";
-import AgendamentoPaciente from "../pages/agendamento-paciente/agendamento-paciente.jsx";
+/* import AgendamentoPaciente from "../pages/agendamento-paciente/agendamento-paciente.jsx"; */
 import DiarioSono from "../pages/diario-sono/diario-sono.jsx";
+
 const router = createBrowserRouter([
     {
         path: "/login",
-        element: <Login />,
+        element: <Login/>,
     },
     {
         path: "/", //direciona para o login
-        element: <Navigate to="/login" replace />,
+        element: <Navigate to="/login" replace/>,
     },
     {
         path: "/acompanhamento-prescritor",
-        element: <AcompanhamentoSemanalPrescritor />,
+        element: <AcompanhamentoSemanalPrescritor/>,
     },
     {
         path: "/acompanhamento-paciente",
-        element: <AcompanhamentoSemanalPaciente />,
+        element: <AcompanhamentoSemanalPaciente/>,
     },
     {
         path: "/dados-consultorio",
-        element: <DadosConsultorio />,
+        element: <DadosConsultorio/>,
     },
     {
         path: "/dashboard-paciente",
-        element: <DashboardPaciente />,
+        element: <DashboardPaciente/>,
     },
     {
         path: "/dashboard-prescritor",
-        element: <DashboardPrescritor />,
+        element: <DashboardPrescritor/>,
     },
     {
         path: "/prescricao",
-        element: <Prescricao />,
+        element: <Prescricao/>,
     },
     {
         path: "/consulta",
-        element: <ConsultaClinica />,
+        element: <ConsultaClinica/>,
     },
     {
         path: "/sign-up",
-        element: <SignUp />,
+        element: <SignUp/>,
     },
-    {
+    /* {
         path: "/agendamento-paciente",
         element: <AgendamentoPaciente />,
-    },
+    }, */
     {
         path: "/diario-sono",
-        element: <DiarioSono />,
+        element: <DiarioSono/>,
     },
 ]);
 
 export default function Routes() {
-    return <RouterProvider router={router} />;
+    return <RouterProvider router={router}/>;
 }

831a95a953c8777dc2e35ff44bae46fa14bc674d
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2025-07-09 02:23:32 -0300

    feat(repository): adicionar método que busca profissional pelo código no PrescriberRepository

--- backend/doisag/src/main/java/dev/uffs/doisag/repository/PrescriberRepository.java
+++ backend/doisag/src/main/java/dev/uffs/doisag/repository/PrescriberRepository.java
@@ -4,6 +4,11 @@ import dev.uffs.doisag.model.Prescriber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PrescriberRepository extends JpaRepository<Prescriber, Long> {
+
+    // método para buscar um prescritor pelo código profissional
+    Optional<Prescriber> findByProfessionalCode(String professionalCode);
 }

e5d413bc74a7608549a5b465b3f6c6b4b82f96cd
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2025-07-12 16:44:49 -0300

    fix(errors): melhora o tratamento de parâmetros inválidos, implementei modificação para capturar nova exceção

--- backend/doisag/src/main/java/dev/uffs/doisag/infra/ErrorHandler.java
+++ backend/doisag/src/main/java/dev/uffs/doisag/infra/ErrorHandler.java
@@ -10,16 +10,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    // trata erros de entidade não encontrada
+    // trata erros de entidade não encontrada (404)
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handleNotFound() {
         return ResponseEntity.notFound().build();
     }
 
-    // trata os nossos erros de validação de negócio
+    // trata os nossos erros de validação de negócio (400)
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity handleBusinessValidation(ValidationException ex) {
         // retorna um 400 bad request com a mensagem da exceção
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+    // trata erros de argumento inválido (400)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleIllegalArgument(IllegalArgumentException ex) {
+        // retorna um 400 bad request com a mensagem da exceção pro giancarlo ficar happy
+        // e o front sabe exatamente qual parâmetro mandou errado
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 }
